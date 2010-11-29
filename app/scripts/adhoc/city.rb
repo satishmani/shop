68368746bf1673a4d4d22ea49ec60da328047e58
Satish Mani <satish.mani@gmail.com>
2010-11-29 16:08:24 +0530

    cahnges to use reduce loops

--- app/scripts/adhoc/city.rb
+++ app/scripts/adhoc/city.rb
@@ -1,6 +1,15 @@
+require 'pp'
 cys = File.open("city.txt"){ |f| f.readlines }
-for cy in cys
-  if cy and cy.chomp and cy.chomp.strip != ""
-     puts "#{cy.gsub('"','').split(',')[0].chomp.strip.split(/\s+/).each{|word| word.capitalize!}.join(' ')}, #{cy.gsub('"','').split(',')[1].chomp.strip.split(/\s+/).each{|word| word.capitalize!}.join(' ')}, IN" if cy.gsub('"','').split(',')[1].chomp.strip != ""
-  end
-end 
+# begin ** -ve criteria **
+   ncriterion = lambda {|cx| cx == nil or cx.chomp == nil or cx.chomp.strip == "" or cx.gsub('"','').split(',')[1].chomp.strip == ""}
+# end ** -ve criteria **
+# begin ** +ve criteria **
+   pcriterion = lambda {|cx| cx != nil and cx.chomp != nil and cx.chomp.strip != "" and cx.gsub('"','').split(',')[1].chomp.strip != ""}
+# end ** +ve criteria **
+
+#ncys = cys.select &ncriterion
+ncys = cys.select &pcriterion
+
+for cy in ncys
+     puts "#{cy.gsub('"','').split(',')[0].chomp.strip.split(/\s+/).each{|word| word.capitalize!}.join(' ')}, #{cy.gsub('"','').split(',')[1].chomp.strip.split(/\s+/).each{|word| word.capitalize!}.join(' ')}, IN"
+end
